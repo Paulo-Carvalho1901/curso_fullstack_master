df2ef2bde804b2c4f277e1066dda693f0b6807a9
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-19 00:24:28 -0300

    variaveis e suas caracteristicas

---
+++ Python/01_variavel_tipo_dados/03_variaveis.py
@@ -0,0 +1,34 @@
+# variavel
+x = 5 # x recebe o valor de 5
+y = 10.4 
+z = 1+2j
+
+w = 'Paulo'
+w = "Paulo"
+
+# Os nomes de variavel pode ir de A-Z e 0-9 _
+
+# altura, Altura, ALTURA, são variaveis diferentes 
+# O python diferencia caixa alta, sendo case-sensitive
+
+h, j, l = 1, 2, 3
+print(h)
+print(j)
+print(l)
+
+a = b = c = ' Faz curso de PYTHO3'
+print(a)
+print(b)
+print(c)
+
+
+nome =  w + a
+print(nome)
+
+soma = x + j
+print(soma)
+
+# Aqui vemos que python é de uma tipagem dinamica e forte
+
+soma = w + h
+# print(soma)
