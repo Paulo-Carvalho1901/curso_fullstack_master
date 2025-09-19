88c94f0cbe5e2c5703c7bf5b091be40ee4c24340
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-19 15:44:01 -0300

    alguns exemplos dos operadores aritméticos

--- Python/02_Estrutura_logica_condicional/01_operadores.py
+++ Python/02_Estrutura_logica_condicional/01_operadores.py
@@ -12,3 +12,18 @@ y = 3
 # Operador da subtração -
 # Operador da multiplicação *
 # Operador da divisão /
+
+
+z = x + y # soma
+print(z)
+
+z = x - y # sybtração
+print(z)
+
+z = x * y # multiplicação
+print(z)
+
+z = x / y
+divisao = 1 / 2
+print(z)
+print(divisao)
