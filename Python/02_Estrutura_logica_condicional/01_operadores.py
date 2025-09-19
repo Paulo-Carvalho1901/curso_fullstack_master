d63e66b552a24c3091500a02e06b8434ce96c5fe
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-19 15:38:03 -0300

    introdução aos operadores aritmeticos

---
+++ Python/02_Estrutura_logica_condicional/01_operadores.py
@@ -0,0 +1,14 @@
+"""
+Operadores aritméticos
+
+"""
+
+x = 5
+y = 3
+
+# Operadores aritméticos
+
+# Operador da soma/adição +
+# Operador da subtração -
+# Operador da multiplicação *
+# Operador da divisão /
