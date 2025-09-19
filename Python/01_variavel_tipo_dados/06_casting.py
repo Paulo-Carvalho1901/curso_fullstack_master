b4cbdc9ef220d253716f7269d59a9281da4fc418
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-19 14:54:34 -0300

    intro sobre casting convertendo tipos de dados

---
+++ Python/01_variavel_tipo_dados/06_casting.py
@@ -0,0 +1,29 @@
+# casting
+
+#int
+
+x = 2
+y = int(2.8)
+z = int('2')
+
+print(x, y, z)
+
+print()
+# float
+
+a = float(2)
+b = float('2.1')
+c = float('2')
+
+print(a, b, c)
+
+print()
+# str
+
+t = 's1'
+r = str(2)
+z = str(2.3)
+
+print('A variavel t é do tipo:', type(t))
+print('A variavel r é do tipo:', type(r))
+print('A variavel z é do tipo:', type(z))
