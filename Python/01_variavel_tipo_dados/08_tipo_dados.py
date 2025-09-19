71b4899f43c9963139f4e173e59f00159b145f4b
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-19 15:14:21 -0300

    tipos de dados

---
+++ Python/01_variavel_tipo_dados/08_tipo_dados.py
@@ -0,0 +1,20 @@
+"""
+Variáveis e os Tipos de dados
+
+Tipos de dados:
+
+Tipo de texto: str
+Tipo númerico: int, float, e o complex
+
+Tipo de sequência: int, tuple, range
+Tipo de Mapeamento: dict
+Tipo Set: set, fronzenset
+
+Tipo Booleano: bool
+Tipo binario: bytes, bytearray, memoryview
+
+print()
+type()
+casting, flot, int, str
+
+"""
