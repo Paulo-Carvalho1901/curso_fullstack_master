e75b0807c71ecb566527534ab47256c20d762fd5
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-19 14:11:16 -0300

    introdução sobre strings e seus metodos basicos

---
+++ Python/01_variavel_tipo_dados/04_string.py
@@ -0,0 +1,30 @@
+# string em Python
+
+print("olá")
+print('ola')
+
+w = 'Curso de Python3'
+print(w)
+
+
+"""comentarios"""
+
+a = """ Ola, esse é 
+o curso de python"""
+print(a)
+
+# Metodos da string
+
+b = " Olá mundo!                 "
+print(b.strip()) # cortando os espaços de uma string
+
+c = 'OLÁ MUNDO!'
+print(c.lower()) # deixando letras em minusculas
+
+d = 'olá mundo!'
+print(d.upper()) # deixando letras em maiuscula
+
+e = 'Olá'
+f = 'mundo!'
+g = e + ' ' + f
+print(g)
