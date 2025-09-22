562856cc8f04b4943b125b5c2c4dc6f68d649455
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-22 16:02:08 -0300

    introdução ao operador logico or

--- Python/02_Estrutura_logica_condicional/02_booleano.py
+++ Python/02_Estrutura_logica_condicional/02_booleano.py
@@ -26,4 +26,11 @@ vardadeiro e Falso - True and False = False
 Falso e verdadeiro - Falso and True = False
 Falso e Falso - False and False = False
 
+Utilizando 'OR'
+
+verdadeiro e verdadeiro - True and True = True 
+vardadeiro e Falso - True and False = True
+Falso e verdadeiro - Falso and True = True
+Falso e Falso - False and False = False
+
 """
