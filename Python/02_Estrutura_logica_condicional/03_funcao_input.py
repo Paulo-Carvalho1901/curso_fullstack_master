e4e7c27ec71716efa7a520c650e1debf44f6d81c
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-22 16:48:25 -0300

    função print introdução

---
+++ Python/02_Estrutura_logica_condicional/03_funcao_input.py
@@ -0,0 +1,9 @@
+"""
+Função input() - Função para receber dados do usuário
+
+"""
+
+nome = input('Qual é seu nome?')
+# print('Olá, '+ nome)
+idade = input('Qual a sua idade? ')
+print('Seu nome é', nome, 'e sua idade é', idade)
