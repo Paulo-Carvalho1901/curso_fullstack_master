ea0ae5fd9f4033af12fbee3c522029302d8763b0
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-22 20:40:49 -0300

    operadores de comparação

---
+++ Python/02_Estrutura_logica_condicional/04_operadores_comparacao.py
@@ -0,0 +1,21 @@
+# Operadores de comparação
+
+# Operador da igualdade (==)
+# Operador da diferença (!=)
+# Operador maior que (>)
+# Operador menor que (<)
+# Operador maior que e igual a (>=)
+# Operador menor que e igual a (<=)
+
+x = 3
+y = 5
+z = 3.0
+w = 8
+
+print(x == y)
+print(x == z)
+print(x != z)
+print(x < y)
+print(x > y)
+print(x >= y)
+print(x <= y)
