e96dfdb212d2f3150a23f4f451ad0b71cb00b0b6
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-22 12:07:20 -0300

    Introdução a lógica booleana

---
+++ Python/02_Estrutura_logica_condicional/02_booleano.py
@@ -0,0 +1,17 @@
+"""
+Operadores logicos e o tipo booleano
+
+Em ciencia da computação, booleano, ou lógico,
+é um tipo de dado primitivo que possui dois valores,
+que podem ser considerados com (0 ou 1), false ou verdadeiro
+
+chamado booleano em homenagem a George Boole
+
+"""
+
+# Tipos Boolean: True or False
+
+# operadores lógicos
+# and (e)
+# or (ou)
+# not (negação)
