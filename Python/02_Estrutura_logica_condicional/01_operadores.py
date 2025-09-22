f5ca8770042f854665d8afc0e0aa1b39c5a36036
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-22 02:39:17 -0300

    mais sobre tipos operadores aritmeticos

--- Python/02_Estrutura_logica_condicional/01_operadores.py
+++ Python/02_Estrutura_logica_condicional/01_operadores.py
@@ -12,7 +12,9 @@ y = 3
 # Operador da subtração -
 # Operador da multiplicação *
 # Operador da divisão /
-
+# Operador do resto %
+# Operador da exponenciação **
+# Operador da divisão inteira // 
 
 z = x + y # soma
 print(z)
@@ -27,3 +29,21 @@ z = x / y
 divisao = 1 / 2
 print(z)
 print(divisao)
+
+resto_1 = 1 % 2
+print(resto_1)
+
+exp_01 = 2 ** 3
+print(exp_01)
+
+div_01 = 4 / 2
+print(div_01)
+
+div_02 = 1 / 2
+print(div_02)
+
+div_01_int = 4 // 2
+print(div_01_int)
+
+div_02_int = 1 // 2
+print(div_02_int)
