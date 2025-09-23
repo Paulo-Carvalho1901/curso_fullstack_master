f789b213fe9be5b81c255077a099d49a628efd15
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-22 21:58:53 -0300

    resolvendo o primeiro exerccio e

---
+++ Python/exercicio/01_exercicio.py
@@ -0,0 +1,26 @@
+"""
+Exercicio - Python
+
+Observação - Todos os exercícios devem utilizar a função input
+de forma que o usuário possa determina os dados de entrada 
+
+01 - área de um retangulo
+02 - área de um quadrado
+03 - se o produto que você quer avaliar custo (??) reais 
+qual será o valor do mesmo com desconto de (??)
+04 - área de um circulo
+05 - conversão de reais para dorlar
+06 - conversão de dolar para real
+"""
+
+print('=-' * 30)
+# exercicio 01 - área do retangulo
+
+
+print('Calcule a áreea de um retangulo.')
+
+base = input('Qual o tamnho da base do seu retangulo')
+altura = input('Qual o tamanho da altura do seu regulamento.')
+area = float(base) * float(altura)
+
+print('fO seu retangulo possui area {area}')
