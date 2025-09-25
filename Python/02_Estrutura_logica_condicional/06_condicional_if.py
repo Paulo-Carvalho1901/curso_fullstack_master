3fa5e9bd4482ded55687a086e3c0a7dbb6b94cd2
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-24 21:17:30 -0300

    introdução ao bloco condicional

---
+++ Python/02_Estrutura_logica_condicional/06_condicional_if.py
@@ -0,0 +1,13 @@
+"""
+Python - Comando de controle condicional
+
+if, else, e elif -> (else if)
+
+"""
+
+x = 3
+y = 8
+
+if y > x:
+    print('Dentro bloco')
+print('Código fora do bloco condicional.')
