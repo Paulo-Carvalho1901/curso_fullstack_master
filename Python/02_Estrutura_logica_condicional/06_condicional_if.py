d8f0d0ea7a4b69fe54b6b298f9e1df312671e02d
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-24 21:35:54 -0300

    estrutura condicional if elif else

--- Python/02_Estrutura_logica_condicional/06_condicional_if.py
+++ Python/02_Estrutura_logica_condicional/06_condicional_if.py
@@ -5,9 +5,17 @@ if, else, e elif -> (else if)
 
 """
 
-x = 3
-y = 8
+x = 4
+y = 4
 
 if y > x:
-    print('Dentro bloco')
+    print('Y é maior do que X.')
+elif y < x:
+    print('Y é menor do que X.')
+elif y == x:
+    print('Y é igual a X')
 print('Código fora do bloco condicional.')
+
+print('é maior', y > x)
+print('é menor', y < x)
+print('é igual', y == x)
